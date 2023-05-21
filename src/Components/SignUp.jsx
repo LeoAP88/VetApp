b90427b55b7c10d1fa80bd0555776e39f17b5c10
almendra dominguez <almendradominguez97@gmail.com>
2023-05-21 16:38:18 -0300

    ejemplo

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -2,9 +2,37 @@ import "./SignUp.css"
 
 const SignUp = () => {
     return(
-        <div id='signup'>
-            HERE GOES A FORM
-        </div>
+        <div class="container">
+      <div class="center">
+          <h1>Register</h1>
+          <form method="POST" action="">
+              <div class="txt_field">
+                  <input type="text" name="name" required></input>
+                  <span></span>
+                  <label>Name</label>
+              </div>
+              <div class="txt_field">
+                  <input type="email" name="email" required></input>
+                  <span></span>
+                  <label>Email</label>
+              </div>
+              <div class="txt_field">
+                  <input type="password" name="password" required></input>
+                  <span></span>
+                  <label>Password</label>
+              </div>
+              <div class="txt_field">
+                  <input type="password" name="cpassword" required></input>
+                  <span></span>
+                  <label>Confirm Password</label>
+              </div>
+              <input name="submit" className="signIn" type="Submit" value="Sign Up"></input>
+              <div class="signup_link">
+                  Have an Account ? <a href="loginForm.php">Login Here</a>
+              </div>
+          </form>
+      </div>
+  </div>
     );
 }
 

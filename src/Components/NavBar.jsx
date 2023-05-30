cca5bcfabd73d6d50587ccfe9fc0949707914d9a
almendra dominguez <almendradominguez97@gmail.com>
2023-05-30 11:13:32 -0300

    mod_navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -16,10 +16,13 @@ const NavBar = ({ onThemeChange }) => {
                 <Link to="/tienda" className="nav-link nav-item">Tienda</Link>
                 <Link to="/adopciones" className="nav-link nav-item">Adopciones</Link>
                 <Link to="/turnos" className="nav-link nav-item">Turnos</Link>
+                <Link to="/" className="nav-link nav-item"><CgProfile/></Link>
+                <Link to="/" className="nav-link nav-item"><MdOutlineLocalGroceryStore/></Link>
+                <a href="https://www.instagram.com/" target="_blank" style={{color: "white", paddingRight:"10px"}}><BsInstagram /></a>
+                <a href="https://es-la.facebook.com/" target="_blank" style={{color: "white", paddingRight:"10px"}}><BsFacebook /></a>
             </div>
-            <ToggleSwitch onToggle={onThemeChange}></ToggleSwitch>
         </nav>
-    );
+    )
 }
 
 export default NavBar;

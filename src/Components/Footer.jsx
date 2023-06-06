61925f174f55edacd2d95d3f2e64ec02c2517c8e
almendra dominguez <almendradominguez97@gmail.com>
2023-06-06 00:27:20 -0300

    mod_css_inicio

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,9 +2,13 @@ import './Footer.css'
 
 const Footer = () => {
     return(
+        <>
         <div id="footer">
-            <p>Dirección: Av. Siempreviva 1500<br />Tel: 5050-5050 <br />© 2023 VeterinariaX. Todos los derechos reservados. </p>
+            <p>Av. Siempreviva 153, CABA<br />(011) 5050-5050 <br />huellitas@mail.com <br /> © 2023 VeterinariaX - Todos los derechos reservados. </p>
         </div>
+
+        {/* <div>Seguinos en redes!</div> */}
+        </>
     );
 }
 

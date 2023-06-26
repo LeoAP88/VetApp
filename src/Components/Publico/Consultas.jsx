73a757c6d0be2e5f86631df561cbfb0d99b574c7
almendra dominguez <almendradominguez97@gmail.com>
2023-06-26 20:35:50 -0300

    cambios_css_consultas_inicio

--- src/Components/Publico/Consultas.jsx
+++ src/Components/Publico/Consultas.jsx
@@ -1,23 +1,28 @@
 import "./Consultas.css";
-import { BsInstagram, BsFacebook } from 'react-icons/all';
+import { AiFillInstagram, BsFacebook, BsTelephone, GrLocation, BsEnvelope } from 'react-icons/all';
 
 const Consultas = () => {
     return (
         <>
-            <h1>¿Tenés alguna duda?</h1>
+            <h1 className="titulo_consultas">¿Tenés alguna duda?</h1>
             <p className="texto-consultas">Comunicate con nosotros por consultas o presupuestos y te responderemos a la brevedad.</p>
 
             <section className="section_consultas">
                 <div className="container_redes">
                     <div className="container_texto_redes">
-                        <p className="texto_redes">Seguinos en nuestras redes</p>
+                        <p className="texto_redes">Seguinos en nuestras redes!</p>
                     </div>
                     <div className="container_iconos_redes">
-                        <a href="https://www.instagram.com/" target="_blank" className="icono_redes"><BsInstagram /></a>
-                        <a href="https://es-la.facebook.com/" target="_blank" className="icono_redes"><BsFacebook /></a>
+                        <a href="https://www.instagram.com/" target="_blank" className="icono_redes"><AiFillInstagram size={60}/></a>
+                        <a href="https://es-la.facebook.com/" target="_blank" className="icono_redes"><BsFacebook size={46} /></a>
+                    </div>
+                    <div className="datos">
+                    <div className="dato_1"><span><BsTelephone size={35} className="icono_dato"/>(011) 50505050</span></div>
+                    <div className="dato"><span><GrLocation size={35} className="icono_dato"/>Av.Siempreviva 123, CABA</span></div>
+                    <div className="dato"><span><BsEnvelope size={35} className="icono_dato"/> huellitas@gmail.com</span></div>
+                    <div className="dato"><iframe  id="mapa" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30661.037707976815!2d-58.4930456391672!3d-34.62141261523201!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcc95874c5f32d%3A0xe6eae433d07a3d57!2sav%20siempre%20viva!5e0!3m2!1ses!2sar!4v1687820007260!5m2!1ses!2sar"></iframe></div>
                     </div>
                 </div>
-
                 <div className="formContainer">
                     <form className="consultas_form" action="mailto:veterinaria@gmail.com" method='POST'>
                         <div className="form-group">
@@ -26,7 +31,7 @@ const Consultas = () => {
                         </div>
                         <div className="form-group">
                             <label htmlFor="email">Email</label>
-                            <input type="email" className="form-control-Consultas" name='email' id="email" placeholder="Email..." />
+                            <input type="email" className="form-control-Consultas" name='email' id="email" placeholder="ejemplo@mail.com" />
                         </div>
                         <div className="form-group">
                             <label htmlFor="mensaje">Mensaje</label>

d2f6e1ef331c0cbd6101aa0c7e3abf0645200aa6
Ezequiel Gonzalez <eze5025@gmail.com>
2023-07-01 22:14:20 -0300

    cambios estilo quienesSomos y semantica

--- src/Components/Publico/QuienesSomos.jsx
+++ src/Components/Publico/QuienesSomos.jsx
@@ -1,132 +1,148 @@
-import Carousel from 'react-bootstrap/Carousel';
+import { Card, Carousel } from 'react-bootstrap';
 import "./QuienesSomos.css"
 
 
 
 
+
 const QuienesSomos = () => {
-    
 
     return(
-     
+      <div id='quienes-somos-container'>
+        <div>
+            <Carousel 
+            id="carousel-quienes-somos" 
+            variant="dark"
+            interval={5000}
+            >
+              <Carousel.Item>
+                <img
+                  className="img-fluid oscurecer"
+                  src="./src/assets/img/1.jpeg"
+                  alt="Responsive image"
+                />
+                <Carousel.Caption>
+                  <div className="txt">
+                    <h1>Somos Huellitas</h1>
+                    <p>
+                      Un centro médico de atención integral para mascotas.
+                      <br /> Desde el año 2019 ofrecemos el servicio de <br />
+                      "Historia Clínica Digital"
+                      <br />
+                      para que puedas tener un registro de todas <br /> las
+                      consultas en un solo lugar
+                    </p> 
+                  </div>
+                </Carousel.Caption>
+              </Carousel.Item>
+            
+              <Carousel.Item>
+                <img
+                  className="img-fluid oscurecer"
+                  src="./src/assets/img/2.jpeg"
+                  alt="Responsive image"
+                />
 
-      <div id='quienes-somos-container'> 
-         <div>
-        <Carousel variant="dark"> 
-      <Carousel.Item>
-        <img
-          className="img-fluid"
-          src="./src/assets/img/1.jpeg"
-          alt="Responsive image"
-        />
-        <Carousel.Caption>
-
-          <div className="txt">
-          <h1>Somos Huellitas</h1>
-          <p>
-                  Un centro médico de atención integral para mascotas.
-                  <br /> Desde el año 2019 ofrecemos el servicio de <br />
-                  "Historia Clínica Digital"
-                  <br />
-                  para que puedas tener un registro de todas <br /> las
-                  consultas en un solo lugar
-                </p> 
-          </div>
-
-
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="img-fluid"
-          src="./src/assets/img/2.jpeg"
-          alt="Responsive image"
-        />
-
-        <Carousel.Caption>
-         <div className="pa">
-         <p>
-                  Contamos con profesionales calificados y<br /> equipamiento.
-                  Sabemos que la tecnología
-                  <br />
-                  es fundamental para el crecimiento de <br />
-                  las instituciones médicas y es una de <br />
-                  nuestras fortalezas.
-                </p>
-          </div>
-
-        </Carousel.Caption>
-      </Carousel.Item>
+                <Carousel.Caption>
+                  <div className="pa">
+                    <p>
+                      Contamos con profesionales calificados y<br /> equipamiento.
+                      Sabemos que la tecnología
+                      <br />
+                      es fundamental para el crecimiento de <br />
+                      las instituciones médicas y es una de <br />
+                      nuestras fortalezas.
+                    </p>
+                  </div>
+                </Carousel.Caption>
+              </Carousel.Item>
       
-      <Carousel.Item>
-        <img
-          className="img-fluid"
-          src="./src/assets/img/vetdog.jpg"
-          alt="Responsive image"
-        />
-        <Carousel.Caption>
-          <div className="txt2">
-          <h2>Nuestras especialidades son:</h2>
-                <h4>Clínica Médica / Nutrición / Cirugía General</h4>
-                <h4>Diagnóstico por imágenes / Laboratorio </h4>
-                <h4>Oncologia / Vacunación</h4>
-          </div>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-    </div> 
+              <Carousel.Item>
+                <img
+                  className="img-fluid oscurecer"
+                  src="./src/assets/img/vetdog.jpg"
+                  alt="Responsive image"
+                />  
+                <Carousel.Caption>
+                  <div className="txt2">
+                    <h2>Nuestras especialidades son:</h2>
+                    <h4>Clínica Médica / Nutrición / Cirugía General</h4>
+                    <h4>Diagnóstico por imágenes / Laboratorio </h4>
+                    <h4>Oncologia / Vacunación</h4>
+                  </div>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+        </div> 
       
-      <div>
-    <h3 className="esp"><img src="../src/assets/img/logoBlanco.jpg"
-     alt="Responsive image" className="logoBlanco"/> Nuestro equipo </h3>
-    
-    <div className="row row-cols-1 row-cols-md-2 g-4">
-  <div className="col">
-    <div className="card">
-      <img src="./src/assets/img/5.jpeg" className="card-img-top" alt="Responsive image"/>
-      <div className="card-body">
+        <div>
 
-        <h5 className="card-title"> Dra. Patricia Hernández y Dr. Nicolás Sáenz</h5>
-        <p className="card-text">Clínica Médica</p>
-      </div>
-    </div>
-  </div>
+          <h3 className="esp">
+            <img src="../src/assets/img/logoBlanco.jpg" alt="Responsive image" className="logoBlanco"/>
+            Nuestro equipo
+          </h3>
+      
+          <div className="row row-cols-1 row-cols-md-2 g-4">
 
-  <div className="col">
-    <div className="card">
-      <img src="./src/assets/img/4.jpeg" className="card-img-top" alt="Responsive image"/>
-      <div className="card-body">
-        <h5 className="card-title">Dra. Carla Sevilla</h5>
-        <p className="card-text">Especialista en Nutrición.</p>
-      </div>
-    </div>
-  </div>
+            <div className="col">
+            <Card>
+              <Card.Img variant='top' src="./src/assets/img/5.jpeg"/>
+              <Card.Body>
+                <Card.Title>
+                  Dra. Patricia Hernández y Dr. Nicolás Sáenz
+                </Card.Title>
+                <Card.Text>
+                  Clínica Médica
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            </div>
 
-  <div className="col">
-    <div className="card">
-      <img src="./src/assets/img/dx2.jpg" className="card-img-top" alt="Responsive image"/>
-      <div className="card-body">
-        <h5 className="card-title">Dr. Carlos Spina</h5>
-        <p className="card-text">Traumatología y Diagnóstico por Imagénes.</p>
-      </div>
-    </div>
-  </div>
+            <div className="col">
+            <Card>
+              <Card.Img variant='top' src="./src/assets/img/4.jpeg"/>
+              <Card.Body>
+                <Card.Title>
+                  Dra. Carla Sevilla
+                </Card.Title>
+                <Card.Text>
+                  Especialista en Nutrición.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            </div>
 
-  <div className="col">
-    <div className="card">
-      <img src="./src/assets/img/RV.jpg" className="card-img-top" alt="Responsive image"/>
-      <div className="card-body">
-        <h5 className="card-title"> Dra. Andrea Diaz y Dr. Juan Goncalves</h5>
-        <p className="card-text">Cirugia General</p>
+            <div className="col">
+            <Card>
+              <Card.Img variant='top' src="./src/assets/img/dx2.jpg"/>
+              <Card.Body>
+                <Card.Title>
+                  Dr. Carlos Spina
+                </Card.Title>
+                <Card.Text>
+                  Traumatología y Diagnóstico por Imagénes.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            </div>
+
+            <div className="col">
+            <Card>
+              <Card.Img variant='top' src="./src/assets/img/RV.jpg"/>
+              <Card.Body>
+                <Card.Title>
+                  Dra. Andrea Diaz y Dr. Juan Goncalves
+                </Card.Title>
+                <Card.Text>
+                  Cirugia General
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            </div>
+
+          </div>
         </div>
-      </div>
-    </div>
-  </div>
-  </div>
-  </div>
- 
-
-    
+      </div>   
     )
   
 }

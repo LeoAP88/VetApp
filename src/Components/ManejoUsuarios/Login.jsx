3c37d0e76be3fc6356072295ff2c17a188fa735d
leogd10 <leonardoaprea@gmail.com>
2023-07-02 22:19:55 -0300

    Validacion de campos

--- src/Components/ManejoUsuarios/Login.jsx
+++ src/Components/ManejoUsuarios/Login.jsx
@@ -1,52 +1,82 @@
-import "./Login.css"
-import {Link, useNavigate, useOutletContext} from "react-router-dom"
-import {useState} from "react"
+import "./Login.css";
+import { Link, useNavigate, useOutletContext } from "react-router-dom";
+import { useState } from "react";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import "firebase/auth"
-import {auth} from "../firebaseConfig/firebase"
-
+import "firebase/auth";
+import { auth } from "../firebaseConfig/firebase";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 const Login = () => {
+  const mySwal = withReactContent(Swal);
+  const navigate = useNavigate();
 
+  const auth = getAuth();
 
-    const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const auth = getAuth();
+  const loginSubmit = (e) => {
+    e.preventDefault();
 
-    const [email,setEmail]=useState("");
-    const [password,setPassword]=useState("");
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        console.log("Ingreso correcto");
 
-    const loginSubmit= (e) =>{
-        e.preventDefault();
+        navigate("/");
+      })
+      .catch((error) => {
+        // alert(`Error: ${error.code} - ${error.message}\nHa habido un error!`);
+        Swal.fire({
+          icon: "error",
+          title: "Error",
+          text: "El usuario ingresado no existe",
+        });
+      });
+  };
+  return (
+    <div id="contenedor">
+      <div id="login">
+        <div className="titulo_login"> Ingresa a tu cuenta</div>
+        <form id="loginform" onSubmit={loginSubmit}>
+          <label htmlFor="email" className="log">
+            Email
+          </label>
+          <input
+            id="usuario"
+            type="email"
+            className="log"
+            name="email"
+            placeholder="ejemplo@email.com"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
 
-        signInWithEmailAndPassword(auth, email, password)
-            .then(()=>{
-                console.log("Ingreso correcto");
-                
-                navigate("/");
-            })
-            .catch((error)=>{
-                alert(`Error: ${error.code} - ${error.message}\nHa habido un error!`)
-            })   
-    }
-    return(
-        <div id="contenedor">
-                <div id="login">
-                <div className="titulo_login"> Ingresa a tu cuenta</div>
-                    <form id="loginform" onSubmit={loginSubmit}>
-                        <label htmlFor="email" className="log">Email</label>
-                        <input id="usuario" type="email" className="log" name="email" placeholder="ejemplo@email.com" required onChange={(e) =>setEmail(e.target.value)}></input>
-
-                        <label htmlFor="password" className="log" >Contraseña</label>
-                        <input id="password" type="password" className="log" placeholder="Contraseña" name="password" required onChange={(e) =>setPassword(e.target.value)}></input>
-                        <a href="#"className="log1" id="cont">¿Olvidaste tu contraseña?</a>
-                        <button type="submit" className="log" onSubmit={loginSubmit}>Ingresar</button>
-                        <Link to={"/register"} className="log1">¿No tienes Cuenta? <b>Registrate</b></Link>
-                    </form>
-                </div>
-            </div>
-        
-    );
-}
+          <label htmlFor="password" className="log">
+            Contraseña
+          </label>
+          <input
+            id="password"
+            type="password"
+            className="log"
+            placeholder="Contraseña"
+            name="password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+          <a href="#" className="log1" id="cont">
+            ¿Olvidaste tu contraseña?
+          </a>
+          <button type="submit" className="log" onSubmit={loginSubmit}>
+            Ingresar
+          </button>
+          <Link to={"/register"} className="log1">
+            ¿No tienes Cuenta? <b>Registrate</b>
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
--- src/Components/ManejoUsuarios/Registro.jsx
+++ src/Components/ManejoUsuarios/Registro.jsx
@@ -1,76 +1,141 @@
-import "./Registro.css"
-import { Link, useNavigate } from "react-router-dom"
+import "./Registro.css";
+import { Link, useNavigate } from "react-router-dom";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
-import { db } from "../firebaseConfig/firebase.jsx"
+import { db } from "../firebaseConfig/firebase.jsx";
 import { collection, doc, setDoc } from "firebase/firestore";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
+const mySwal = withReactContent(Swal);
 
 const Registro = () => {
-    const auth = getAuth();
-    const navigate = useNavigate();;
-    const clientesCollection = collection(db, "Clientes");
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const clientesCollection = collection(db, "Clientes");
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [nombre, setNombre] = useState("");
-    const [apellido, setApellido] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [nombre, setNombre] = useState("");
+  const [apellido, setApellido] = useState("");
 
-    const submit = () => {
-        createUserWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                //Once the user creation has happened successfully, we can add the currentUser into firestore
-                //with the appropriate details.
-                console.log(userCredential);
-                const user = userCredential.user;
-                console.log(user);
-                setDoc(doc(db, "Clientes", user.uid), {
-                    Nombre: nombre,
-                    Apellido: apellido,
-                    Email: email,
-                })
-                    //ensure we catch any errors at this stage to advise us if something does go wrong
-                    .catch(error => {
-                        console.log('Something went wrong with added user to firestore: ', error);
-                    })
-                navigate("/");
-            })
-            //we need to catch the whole sign up process if it fails too.
-            .catch(error => {
-                console.log('Something went wrong with sign up: ', error);
-            })
+  const submit = (e) => {
+    e.preventDefault();
+    validarEmail();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        //Once the user creation has happened successfully, we can add the currentUser into firestore
+        //with the appropriate details.
+        console.log(userCredential);
+        const user = userCredential.user;
+        console.log(user);
+        setDoc(doc(db, "Clientes", user.uid), {
+          Nombre: nombre,
+          Apellido: apellido,
+          Email: email,
+        })
+          //ensure we catch any errors at this stage to advise us if something does go wrong
+          .catch((error) => {
+            console.log(
+              "Something went wrong with added user to firestore: ",
+              error
+            );
+          });
+        navigate("/");
+      })
+      //we need to catch the whole sign up process if it fails too.
+      .catch((error) => {
+        console.log("Something went wrong with sign up: ", error);
+      });
+  };
 
+  const validarEmail = () => {
+    let validador_email = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+    if (validador_email.test(email)) {
+      return true;
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Error en email",
+        text: "El formato de email no es correcto",
+      });
+      return false;
     }
+  };
 
+  const PasswdError = () => {
+    if (password.length < 6) {
+      return (
+        <span style={{ color: "red" }}>Se requieren al menos 6 caracteres</span>
+      );
+    }
+  };
 
-    return (
-        <div id="container">
-            <div id="registro">
-                <h1 className="Titulo_registro">Registro</h1>
-                <form method="POST" action="" id="registroform">
-                        <label>Email</label>
-                        <input type="email" name="email" placeholder="ejemplo@email.com" required onChange={(e) => setEmail(e.target.value)}></input>
-                        
-                        <label>Password</label>
-                        <input type="password" name="password" placeholder="Mínimo 6 caracteres" required onChange={(e) => setPassword(e.target.value)}></input>
-                        
-                        <label>Nombre</label>
-                        <input type="nombre" name="nombre" placeholder="Nombre..." required onChange={(e) => setNombre(e.target.value)}></input>
+  return (
+    <div id="container">
+      <div id="registro">
+        <h1 className="Titulo_registro">Registro</h1>
+        <form method="POST" action="" id="registroform">
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="ejemplo@email.com"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
 
-                        <label>Apellido</label>
-                        <input type="apellido" name="apellido" placeholder="Apellido..." required onChange={(e) => setApellido(e.target.value)}></input>
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            // placeholder="Mínimo 6 caracteres"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+          <PasswdError />
 
-                    <Link to={"/"} className="Link">
-                        <button className="btn-registro" type="submit" title="Registrarse" name="Registrarse" disabled={password.length < 6} onClick={submit}>Registrarse</button>
-                    </Link>
-                    <div className="signup_link">
-                        ¿Ya tenes una cuenta? <Link to={"/Login"} className="bold"><b>Ingresá acá</b></Link>
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
-}
+          <label>Nombre</label>
+          <input
+            type="nombre"
+            name="nombre"
+            placeholder="Nombre..."
+            required
+            onChange={(e) => setNombre(e.target.value)}
+          ></input>
+
+          <label>Apellido</label>
+          <input
+            type="apellido"
+            name="apellido"
+            placeholder="Apellido..."
+            required
+            onChange={(e) => setApellido(e.target.value)}
+          ></input>
+
+          <Link to={"/"} className="Link">
+            <button
+              className="btn-registro"
+              type="submit"
+              title="Registrarse"
+              name="Registrarse"
+              disabled={password.length < 6}
+              onClick={submit}
+            >
+              Registrarse
+            </button>
+          </Link>
+          <div className="signup_link">
+            ¿Ya tenes una cuenta?{" "}
+            <Link to={"/Login"} className="bold">
+              <b>Ingresá acá</b>
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 export default Registro;
 
 /*import "./Registro.css"
